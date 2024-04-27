e869c66d1bb20a1e7daa0e2039c1f58c0a2e7cd5
Criki <criterionist@foxmail.com>
2024-04-28 06:41:31 +0800

    finish algorithm7

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -3,7 +3,6 @@
 	This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
 #[derive(Debug)]
 struct Stack<T> {
 	size: usize,
@@ -32,7 +31,12 @@ impl<T> Stack<T> {
 	}
 	fn pop(&mut self) -> Option<T> {
 		// TODO
-		None
+		if 0 == self.size {
+			return None;
+		}
+
+		self.size -= 1;
+		self.data.pop()
 	}
 	fn peek(&self) -> Option<&T> {
 		if 0 == self.size {
@@ -102,7 +106,27 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 fn bracket_match(bracket: &str) -> bool
 {
 	//TODO
-	true
+	let mut stack = Stack::new();
+	for c in bracket.chars() {
+		if [')', ']', '}'].contains(&c) {
+			if stack.is_empty() {
+				return false;
+			}
+			
+			let top = stack.peek().unwrap();
+			if (c == ')' && *top == '(') ||
+			(c == ']' && *top == '[') ||
+			(c == '}' && *top == '{') {
+				stack.pop();
+			} else {
+				return false;
+			}
+		} else if ['(', '[', '{'].contains(&c) {
+			stack.push(c);
+		}
+
+	}
+	stack.is_empty()
 }
 
 #[cfg(test)]
